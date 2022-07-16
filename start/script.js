a5ef48a0e5b6bbb34731de51cfaf8cdc824e8c14
GiovaniiDias <giovani.eb3@gmail.com>
2022-07-16 16:09:16 -0300

    add js no projeto

---
+++ start/script.js
@@ -0,0 +1,17 @@
+function calculateTip(event) {
+    event.preventDefault()
+    let bill = document.getElementById('bill').value
+    let serviceQual = document.getElementById('serviceQual').value
+    let numOfPeople = document.getElementById('people').value
+
+    if (bill == "" | serviceQual == 0 ){
+        alert("Por favor, preencha os valores!")
+        return
+    }
+
+}
+document.getElementById("totalTip").style.display = "none"
+document.getElementById("each").style.display = "none"
+
+
+document.getElementById("tipsForm").addEventListener("submit", calculateTip)
