49ede886d09a5bc543d925d8b2ca2af511322649
GiovaniiDias <giovani.eb3@gmail.com>
2022-07-16 18:09:12 -0300

    finalizei o projeto!

--- start/script.js
+++ start/script.js
@@ -5,11 +5,22 @@ function calculateTip(event) {
     let numOfPeople = document.getElementById('people').value
 
     if (bill == "" | serviceQual == 0 ){
-        alert("Por favor, preencha os valores!")
+        alert("Vou calcular o que? o merda! preencha os valores!")
         return
     }
-
+    if (numOfPeople =="" | numOfPeople <= 1){
+        numOfPeople = 1
+        document.getElementById("each").style.display = "none"
+    } else {
+        document.getElementById("each").style.display = "block"
+    }
+     let total = (bill * serviceQual) / numOfPeople
+     total = total.toFixed(2)
+     document.getElementById("tip").innerHTML = total
+     document.getElementById("totalTip").style.display = "block"
 }
+
+
 document.getElementById("totalTip").style.display = "none"
 document.getElementById("each").style.display = "none"
 
